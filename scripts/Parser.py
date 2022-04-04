394fcbecbe911e054ce4e9f04b78a9287ffcb230
Gonzalo Nicolás <gonnm8@gmail.com>
2022-04-04 18:59:07 +0200

    feat: Adapted scripts and automation module
    Changed so the metrics parser only parses on request. Main module now differs its functionality depending on flags, and does not allow certain combinations. Automation module now calls script right after each oquare call

--- scripts/Parser.py
+++ scripts/Parser.py
@@ -4,15 +4,12 @@ class MetricsParser:
     def __init__(self, metrics_file: str):
         self.tree = ET.parse(metrics_file)
         self.root = self.tree.getroot()
-        self.oquare_value = self.__parse_oquare_value()
-        self.scaled_metrics = self.__parse_scaled_metrics()
-        self.category_metrics = self.__parse_category_metrics()
 
-    def __parse_oquare_value(self) -> float:
+    def parse_oquare_value(self) -> float:
         oquare_value = float(self.root.find('oquareModel').attrib.get('oquareValue'))
         return oquare_value
     
-    def __parse_scaled_metrics(self):
+    def parse_scaled_metrics(self):
         scaled_metrics = self.root.findall('./oquareMetricsScaled/')
         metrics_dict = {}
         for metric in scaled_metrics:
@@ -20,7 +17,7 @@ class MetricsParser:
         
         return metrics_dict
 
-    def __parse_category_metrics(self):
+    def parse_category_metrics(self):
         oquare_model = self.root.findall('oquareModel/')
         oquare_model_dict = {}
         for metric in oquare_model:
@@ -53,12 +50,3 @@ class MetricsParser:
         # 
         # }
         return oquare_model_dict
-    
-    def get_oquare_value(self):
-        return self.oquare_value
-
-    def get_scaled_metrics(self):
-        return self.scaled_metrics
-
-    def get_category_metrics(self):
-        return self.category_metrics
--- scripts/main.py
+++ scripts/main.py
@@ -12,10 +12,11 @@ if __name__ == '__main__':
     plot_models = False
     plot_categories = False
     plot_global = False
-    help_msg = "Uso: {0} -i <folderPath> -o <outputPath> -m || -c || -g".format(argv[0])
+    file = ''
+    help_msg = "Uso: {0} -i <folderPath> -o <outputPath> [-c -f <fileNAME (no extension, no path)>] || [-m -g]".format(argv[0])
 
     try:
-        opts, args = getopt.getopt(argv[1:], "hi:o:mcg", ["help", "input=", "output=", "model", "categories", "global"])
+        opts, args = getopt.getopt(argv[1:], "hi:o:f:mcg", ["help", "input=", "output=", "file=", "model", "categories", "global"])
     except:
         print(help_msg)
         sys.exit(2)
@@ -34,76 +35,87 @@ if __name__ == '__main__':
             plot_categories = True
         elif opt in ("-g", "--global"):
             plot_global = True
+        elif opt in ("-f", "--file"):
+            file = arg
 
     basepath = inputPath + '/temp_results/'
-    oquare_model_values = {}
     graphPlotter = oquareGraphs()
-    with os.scandir(basepath) as entries:
-        for entry in entries:
-            oquare_category_values = {}
-            parsed_metrics = MetricsParser(basepath + entry.name + '/metrics/' + entry.name + ".xml")
-            categories = parsed_metrics.get_category_metrics()
 
-            if plot_categories:
-                for category, values in categories.items():
-                    oquare_category_values[category] = values.get('value')
-                
-                graphPlotter.plot_oquare_categories(oquare_category_values, entry.name)
-            
-            oquare_model_values[entry.name] = parsed_metrics.get_oquare_value()
-    
-    if plot_models:
-        graphPlotter.plot_oquare_values(oquare_model_values)
+    if plot_categories:
+        if not file or plot_global or plot_models:
+            print(help_msg)
+            sys.exit(2)
+        
+        oquare_category_values = {}
+        parsed_metrics = MetricsParser(basepath + file + '/metrics/' + file + '.xml')
+        categories = parsed_metrics.parse_category_metrics()
+        for category, values in categories.items():
+            oquare_category_values[category] = values.get('value')
+        
+        graphPlotter.plot_oquare_categories(oquare_category_values, file)
+        sys.exit(0)
 
-    if plot_global:
-        archive_path = inputPath + '/archives/'
-        oquare_model_values_global = {}
-        entries = os.listdir(archive_path)
-
-        # Si hay menos de 18 resultados archivados, los extraigo todos
-
-        if len(entries) < 18:
-            # Por cada archivado extraigo las carpetas de ontologias
-            for entry in entries:
-                with os.scandir(archive_path + entry + '/') as ontologies:
-                    for ontology in ontologies:
-                        # Si es carpeta (puede haber imagenes de resultados previos)
-                        if ontology.is_dir():
-                            # Si la entrada no existe para esa ontologia, la añado
-                            if not oquare_model_values_global.get(ontology.name):
-                                oquare_model_values_global[ontology.name] = list()
-                            
-                            # Extraigo el oquare_value de esa ontología para ese archivado y la añado a la lista de esa ontologia
-                            parsed_metrics = MetricsParser(ontology.path + '/metrics/' + ontology.name + '.xml')
-                            oquare_model_values_global.get(ontology.name).append(parsed_metrics.get_oquare_value())
-        else:
-            for i in range(len(entries)-18, len(entries)):
-                entry = entries[i]
-                with os.scandir(archive_path + entry + '/') as ontologies:
-                    for ontology in ontologies:
-                        # Si es carpeta (puede haber imagenes de resultados previos)
-                        if ontology.is_dir():
-                            # Si la entrada no existe para esa ontologia, la añado
-                            if not oquare_model_values_global.get(ontology.name):
-                                oquare_model_values_global[ontology.name] = list()
-                            
-                            # Extraigo el oquare_value de esa ontología para ese archivado y la añado a la lista de esa ontologia
-                            parsed_metrics = MetricsParser(ontology.path + '/metrics/' + ontology.name + '.xml')
-                            oquare_model_values_global.get(ontology.name).append(parsed_metrics.get_oquare_value())
-
-        # Saco el oquare_value de las ontologias previas a esta ejecución (almacenadas en results)
-        results_path = inputPath + '/results/'
-        results_entry = os.listdir(results_path)[0]
-        with os.scandir(results_path + results_entry) as entries:
+    else:
+        oquare_model_values = {}
+        
+        with os.scandir(basepath) as entries:
             for entry in entries:
                 if entry.is_dir():
-                    parsed_metrics = MetricsParser(entry.path + '/metrics/' + entry.name + '.xml')
-                    oquare_model_values_global.get(entry.name).append(parsed_metrics.get_oquare_value())  
+                    parsed_metrics = MetricsParser(basepath + entry.name + '/metrics/' + entry.name + ".xml")
+                    oquare_model_values[entry.name] = parsed_metrics.parse_oquare_value()
+        
+        if plot_models:
+            graphPlotter.plot_oquare_values(oquare_model_values)
 
-        # Añado el oquare_value que se ha calculado en esta ejecucion
-        for ontology in oquare_model_values:
-            oquare_model_values_global.get(ontology).append(oquare_model_values.get(ontology))
+        if plot_global:
+            archive_path = inputPath + '/archives/'
+            oquare_model_values_global = {}
+            entries = os.listdir(archive_path)
 
-        # Plot and save
-        print(oquare_model_values_global)
+            # Si hay menos de 18 resultados archivados, los extraigo todos
+
+            if len(entries) < 18:
+                # Por cada archivado extraigo las carpetas de ontologias
+                for entry in entries:
+                    with os.scandir(archive_path + entry + '/') as ontologies:
+                        for ontology in ontologies:
+                            # Si es carpeta (puede haber imagenes de resultados previos)
+                            if ontology.is_dir():
+                                # Si la entrada no existe para esa ontologia, la añado
+                                if not oquare_model_values_global.get(ontology.name):
+                                    oquare_model_values_global[ontology.name] = list()
+                                
+                                # Extraigo el oquare_value de esa ontología para ese archivado y la añado a la lista de esa ontologia
+                                parsed_metrics = MetricsParser(ontology.path + '/metrics/' + ontology.name + '.xml')
+                                oquare_model_values_global.get(ontology.name).append(parsed_metrics.parse_oquare_value())
+            else:
+                for i in range(len(entries)-18, len(entries)):
+                    entry = entries[i]
+                    with os.scandir(archive_path + entry + '/') as ontologies:
+                        for ontology in ontologies:
+                            # Si es carpeta (puede haber imagenes de resultados previos)
+                            if ontology.is_dir():
+                                # Si la entrada no existe para esa ontologia, la añado
+                                if not oquare_model_values_global.get(ontology.name):
+                                    oquare_model_values_global[ontology.name] = list()
+                                
+                                # Extraigo el oquare_value de esa ontología para ese archivado y la añado a la lista de esa ontologia
+                                parsed_metrics = MetricsParser(ontology.path + '/metrics/' + ontology.name + '.xml')
+                                oquare_model_values_global.get(ontology.name).append(parsed_metrics.parse_oquare_value())
+
+            # Saco el oquare_value de las ontologias previas a esta ejecución (almacenadas en results)
+            results_path = inputPath + '/results/'
+            results_entry = os.listdir(results_path)[0]
+            with os.scandir(results_path + results_entry) as entries:
+                for entry in entries:
+                    if entry.is_dir():
+                        parsed_metrics = MetricsParser(entry.path + '/metrics/' + entry.name + '.xml')
+                        oquare_model_values_global.get(entry.name).append(parsed_metrics.parse_oquare_value())  
+
+            # Añado el oquare_value que se ha calculado en esta ejecucion
+            for ontology in oquare_model_values:
+                oquare_model_values_global.get(ontology).append(oquare_model_values.get(ontology))
+
+            # Plot and save
+            print(oquare_model_values_global)
 
