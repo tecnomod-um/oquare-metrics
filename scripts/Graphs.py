f663399ba794228bd17317aae3a8e688a5579623
Gonzalo Nicolás <gonnm8@gmail.com>
2022-04-04 11:23:50 +0200

    feat: modified category plots

--- scripts/Graphs.py
+++ scripts/Graphs.py
@@ -15,12 +15,24 @@ class oquareGraphs:
     
     def plot_oquare_categories(self, data: dict, fileName: str, outputPath: str = 'OQuaRE'):
 
-        plt.figure()
+        plt.figure(figsize=(15.5, 5))
         names = list(data.keys())
         values = list(data.values())
-
         ax = plt.gca()
+        ax.set_title('OQuaRE model category values')
         ax.set_ylim([0, 5])
 
-        plt.bar(names, values)
-        plt.savefig(outputPath + '/temp_results/' + fileName + '/' + fileName + "category_values.png", format="png")
+        ax.tick_params(
+            axis='x',
+            which='both',
+            bottom=False,
+            labelbottom=False
+        )
+        ax.grid()
+        
+        plt.plot(range(len(names)), values, '-ko', mfc='red')
+        
+        for i in range(len(names)):
+            ax.annotate('%s' % names[i], xy=(i - 0.25, values[i] + 0.2), textcoords='data')
+
+        plt.savefig(outputPath + '/temp_results/' + fileName + '/' + fileName + "category_values.png", format="png", bbox_inches='tight')
