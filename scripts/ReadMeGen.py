ecb3eb668195708eb9d84c95d88f945d093f6301
Gonzalo Nicolás <gonnm8@gmail.com>
2022-04-19 11:25:54 +0200

    feat: Updated workflow to make use of param
    Also possibly fixed images not previewing on README.md

--- scripts/ReadMeGen.py
+++ scripts/ReadMeGen.py
@@ -5,7 +5,7 @@ class readmeGen:
     def append_category(self, fileName: str, path: str):
         readme_file = open(path + '/temp_results/'+ fileName + '/' + 'README.md', 'a')
         readme_file.write('# OQuaRE category values\n')
-        readme_file.write('[category values plot](' + fileName + 'category_values.png)\n')
+        readme_file.write('![category values plot](' + fileName + 'category_values.png)\n')
         readme_file.write('Each category has a value on a scale of 1 to 5, indicating how good the ontology is for each category\n')
         readme_file.close()
 
@@ -13,7 +13,7 @@ class readmeGen:
         readme_file = open(path + '/temp_results/README.md', 'a')
         readme_file.write('# OQuaRE metrics results\n')
         readme_file.write('## OQuaRE model value\n')
-        readme_file.write('[OQuaRE model value plot](OQuaRE_model_values.png)\n')
+        readme_file.write('![OQuaRE model value plot](OQuaRE_model_values.png)\n')
         readme_file.write('Represents the quality of the ontology when taking in all the values obtained after evaluation.' 
                         + 'On a scale of 1 to 5, 5 represents the highest quality attainable according to the OQuaRE framework\n')
         readme_file.close()
@@ -21,7 +21,7 @@ class readmeGen:
     def append_oquare_historic(self, path: str, date: str):
         readme_file = open(path + '/results/' + date +'/README.md', 'a')
         readme_file.write('## OQuaRE historic model value\n')
-        readme_file.write('[OQuaRE historic values plot](OQuaRE_historic_model_value.png)\n')
+        readme_file.write('![OQuaRE historic values plot](OQuaRE_historic_model_value.png)\n')
         readme_file.write('Progress of each analyzed ontology across the latest 20 versions on a scale of 1 to 5\n')
         readme_file.close()
 
