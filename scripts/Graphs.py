026eda58beddaeba6fdf58cdaabb0092898bd288
Gonzalo Nicolás <gonnm8@gmail.com>
2022-04-21 13:07:46 +0200

    feat: attempt at modifying bar plot styling

--- scripts/Graphs.py
+++ scripts/Graphs.py
@@ -14,6 +14,7 @@ class oquareGraphs:
             plt.bar(xpos, values)
             plt.xticks(xpos, names, fontsize=8, rotation=90)
             matplotx.show_bar_values("{:.2f}")
+            plt.rc('axes', labelsize=8, labelcolor='black')
             plt.title('OQuaRE model values')
             plt.savefig(outputPath + '/temp_results/OQuaRE_model_values.png', format="png", bbox_inches='tight')
     
