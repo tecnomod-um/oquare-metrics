cb9e22fc0916732fa49fc506573a72219ee10eef
Gonzalo Nicolás <gonnm8@gmail.com>
2022-05-15 19:22:26 +0200

    feat: Updated main module to plot subcategories and their evolution
    - Removed code usage
    - Added a safeguard on initial run before moving and commiting changes, for the case that there is nothing created
    - Changed the way evolution is done. Now it plots the evolution of the desired parts of the metrics, instead of all the evolutions

--- src/main.py
+++ src/main.py
@@ -8,58 +8,64 @@ if __name__ == '__main__':
     input_path = ""
     plot_models = False
     plot_categories = False
-    plot_evolution = False
+    plot_subcategories = False
     plot_metrics = False
+    plot_evolution = False
     file = ''
     ontology_source = ''
-    code = "0"
     date= ""
 
 
     controller = Controller()
 
     try:
-        opts, args = getopt.getopt(argv[1:], "i:s:f:Mcemq:d:", ["input=","source=", "file=", "model", "categories", "evolution", "metrics", "code=", "date="])
+        opts, args = getopt.getopt(argv[1:], "i:s:f:McmSed:", ["input=","source=", "file=", "model", "categories", "subcategories", "metrics", "evolution", "date="])
     except:
         sys.exit(2)
     
     for opt, arg in opts:
         if opt in ("-i", "--input"):
             input_path = arg
+        elif opt in ("-s", "--source"):
+            ontology_source = arg
+        elif opt in ("-f", "--file"):
+            file = arg
         elif opt in ("-M", "--model"):  
             plot_models = True
         elif opt in ("-c", "--categories"):
             plot_categories = True
-        elif opt in ("-f", "--file"):
-            file = arg
-        elif opt in ("-s", "--source"):
-            ontology_source = arg
-        elif opt in ("-e", "--evolution"):
-            plot_evolution = True
+        elif opt in ("-S", "--subcategories"):
+            plot_subcategories = True
         elif opt in ("-m", "--metrics"):
             plot_metrics = True
-        elif opt in ("-q", "--code"):
-            code = arg
+        elif opt in ("-e", "--evolution"):
+            plot_evolution = True
         elif opt in ("-d", "--date"):
             date = arg
 
     temp_path = input_path + '/temp_results/'
 
-    if code != "0":
-        print('Error: ' + code + ", aborted. Check OQuaRE logs")
-        sys.exit()
-
     if plot_categories:
         temp_path += ontology_source + '/' + file + '/' + date 
         controller.handle_categories(temp_path, file)
 
-    elif plot_evolution:
-        controller.handle_category_evolution(file, input_path, ontology_source, date)
-        controller.handle_metrics_evolution(file, input_path, ontology_source, date)
+        if plot_evolution:
+            controller.handle_category_evolution(file, input_path, ontology_source, date)
+
+    elif plot_subcategories:
+        temp_path += ontology_source + '/' + file + '/' + date
+        controller.handle_subcategories(temp_path, file)
+
+        if plot_evolution:
+            controller.handle_subcategory_evolution(file, input_path, ontology_source, date)
+        
     elif plot_models:
         controller.handle_oquare_model(file, input_path, ontology_source, date)
+
     elif plot_metrics:
         temp_path += ontology_source + '/' + file + '/' + date 
         controller.handle_metrics(temp_path, file)
+        if plot_evolution:
+            controller.handle_metrics_evolution(file, input_path, ontology_source, date)
         
 
