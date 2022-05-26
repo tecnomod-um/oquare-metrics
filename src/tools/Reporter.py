df0757291034aeba19d701b30b47e162a18276bc
Gonzalo Nicolás <gonnm8@gmail.com>
2022-05-26 09:35:35 +0200

    feat: styling readme with html and css
    - Most likely broken. Needs revision

--- src/tools/Reporter.py
+++ src/tools/Reporter.py
@@ -4,7 +4,9 @@ class readmeGen:
         readme_file = open(path + '/README.md', 'a')
         readme_file.write('## OQuaRE category values\n')
         readme_file.write('Each category has a value on a scale of 1 to 5, indicating how good the ontology is for each category\n\n')
-        readme_file.write('![category values plot](img/' + file_name + '_category_values.png)\n')
+        readme_file.write('<p align="center" width="100%">\n')
+        readme_file.write('\t<img src="img/' + file_name + '_category_values.png"/>\n')
+        readme_file.write('</p>\n')
         readme_file.close()
 
     def append_oquare_value(self, file_name: str, path: str):
@@ -12,40 +14,53 @@ class readmeGen:
         readme_file.write('## OQuaRE model value\n')
         readme_file.write('Represents the quality of the ontology when taking in all the values obtained after evaluation across the latest 20 versions.' 
                         + 'On a scale of 1 to 5, 5 represents the highest quality attainable according to the OQuaRE framework\n\n')
-        readme_file.write('![OQuaRE model value plot](img/' + file_name + '_OQuaRE_model_values.png)\n')
+        readme_file.write('<p align="center" width="100%">\n')
+        readme_file.write('\t<img src="img/' + file_name + '_OQuaRE_model_values.png"/>\n')
+        readme_file.write('</p>\n')
         readme_file.close()
 
     def append_subcategory(self, file_name: str, path: str, categories: list):
         readme_file = open(path + '/README.md', 'a')
         readme_file.write('## OQuaRE subcategories metrics\n')
         readme_file.write('Each category has a set of subcategories with metrics on a scale of 1 to 5, which makes up the category end value\n\n')
-        
+        readme_file.write('<p align="center" width="100%">\n')
+
         for category in categories:
-            readme_file.write('![' + category + ' metrics plot](img/' + file_name + "_" + category + '_subcategories_metrics.png)\n')
+            readme_file.write('\t<img width="31%" src="img/' + file_name + '_' + category + '_subcategories_metrics.png"/>\n')
+        
+        readme_file.write('</p>\n')
         readme_file.close()
+        
     
     def append_subcategories_evolution(self, file_name: str, path: str, categories: list):
         readme_file = open(path + '/README.md', 'a')
         readme_file.write('## OQuaRE subcategories metrics evolution\n')
         readme_file.write('Evolution of each category subcategories values overtime on a scale of 1 to 5\n\n')
-        
+        readme_file.write('<p align="center" width="100%">\n')
+
         for category in categories:
-            readme_file.write('![' + category + ' metrics plot](img/' + file_name + "_" + category + '_subcategories_evolution.png)\n')
+            readme_file.write('\t<img width="31%" src="img/' + file_name + '_' + category + '_subcategories_evolution.png"/>\n')
+        
+        readme_file.write('</p>\n')
         readme_file.close()
     
     def append_category_evolution(self, file_name: str, path: str, ):
         readme_file = open(path + '/README.md', 'a')
         readme_file.write('## OQuaRE category evolution\n')
         readme_file.write('Evolution of each category overtime on a scale of 1 to 5\n\n')
-        readme_file.write('![category values evolution plot](img/' + file_name + '_categories_evolution.png)\n')
+        readme_file.write('<p align="center" width="100%">\n')
+        readme_file.write('\t<img src="img/' + file_name + '_categories_evolution.png"/>\n')
+        readme_file.write('</p>\n')
         readme_file.close()
 
     def append_metrics(self, file_name: str, path: str): 
         readme_file = open(path + '/README.md', 'a')
         readme_file.write('## OQuaRE metrics values\n')
         readme_file.write('Fine grained metrics, lowest level of ontology analysis provided. Scaled version uses a 1 to 5 scale\n\n')
-        readme_file.write('![metrics plot](img/' + file_name + '_metrics.png)\n')
-        readme_file.write('![scaled metrics plot](img/' + file_name + '_scaled_metrics.png)\n')
+        readme_file.write('<p align="center" width="100%">\n')
+        readme_file.write('\t<img width="45%" src="img/' + file_name + '_metrics.png"/>\n')
+        readme_file.write('\t<img width="45%" src="img/' + file_name + '_scaled_metrics.png"/>\n')
+        readme_file.write('</p>\n')
         readme_file.close()
 
     def append_metrics_evolution(self, file_name: str, path: str, metrics: list):
@@ -54,13 +69,19 @@ class readmeGen:
         readme_file.write('Evolution of each of the 19 metrics obtained from an ontology\n')
         
         for metric in metrics:
+            readme_file.write('<div style="width: 33%; float: left;">')
             readme_file.write('### ' + metric +' evolution\n\n')
-            readme_file.write('![' + metric + ' metrics evolution plot](img/' + file_name + '_' + metric + '_metric_evolution.png)\n\n')
+            readme_file.write('<p align="center" width="100%">\n')
+            readme_file.write('\t<img src="img/' + file_name + '_' + metric + '_metric_evolution.png"/>\n')
+            readme_file.write('</p>\n')
+            readme_file.write
         readme_file.close()
     
     def append_scaled_metrics_evolution(self, file_name: str, path: str):
         readme_file = open(path + '/README.md', 'a')
         readme_file.write('## OQuaRE scaled metrics evolution\n')
         readme_file.write('Evolution of each of the 19 scaled metrics obtained from an ontology\n\n')
-        readme_file.write('![scaled metrics evolution plot](img/' + file_name + '_scaled_metrics_evolution.png)\n')
+        readme_file.write('<p align="center" width="100%">\n')
+        readme_file.write('\t<img src="img/' + file_name + '_scaled_metrics_evolution.png"/>\n')
+        readme_file.write('</p>\n')
         readme_file.close()
