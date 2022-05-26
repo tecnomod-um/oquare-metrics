48729b4695f324f50bb777916778371bdc46b71a
Gonzalo Nicolás <gonnm8@gmail.com>
2022-05-26 20:31:38 +0200

    feat: slightly reduced fig size scaled metrics evo, decreased font size

--- src/tools/Plotter.py
+++ src/tools/Plotter.py
@@ -252,7 +252,7 @@ class oquareGraphs:
         
         with plt.style.context(matplotx.styles.ayu["light"]):
 
-            fig, axs = plt.subplots(4, 5, figsize=(13,13))
+            fig, axs = plt.subplots(4, 5, figsize=(12,12))
             
             row = 0
             col = 0
@@ -262,7 +262,7 @@ class oquareGraphs:
                 dates = data.get(label).keys()
                 axs[row, col].plot(range(len(dates)), values, label=label)
                 axs[row, col].set_xticks(range(len(dates)))
-                axs[row, col].set_xticklabels(range(len(dates)), fontsize=6)
+                axs[row, col].set_xticklabels(range(len(dates)), fontsize=5)
                 axs[row, col].set_ylim(0, 5.5)
                 axs[row, col].set_title(label, fontsize=9)
 
